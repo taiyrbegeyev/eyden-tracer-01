bb8dbc758a9898d5271724c526ebddce98b8d0de
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-09-27 00:14:17 +0200

    :sparkles: introduce initray

--- src/CameraPerspective.h
+++ src/CameraPerspective.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "ICamera.h"
+#include <math.h>
 
 /**
  * @brief Perspective Camera class
@@ -23,14 +24,34 @@ public:
 		, m_pos(pos)
 		, m_dir(dir)
 		, m_up(up)
+		, m_angle(angle)
 	{
 		// --- PUT YOUR CODE HERE ---
+        m_aspect = (float) resolution.width / (float) resolution.height;
+        m_focus = 1 / tan((Pif * angle) / 180);
+        m_xAxis = normalize(dir.cross(up));
+        m_yAxis = normalize(up * (-1));
 	}
 	virtual ~CCameraPerspective(void) = default;
 
 	virtual bool InitRay(float x, float y, Ray& ray) override
 	{
 		// --- PUT YOUR CODE HERE ---
+		float width = getResolution().width;
+		float height = getResolution().height;
+
+        // normalized device coordinates
+        float ndcx = (x + 0.5) / width;
+        float ndcy = (y + 0.5) / height;
+        // screen space coordinates
+        float sscx = (2 * ndcx - 1) * m_aspect;
+        float sscy = 2 * ndcy - 1;
+
+        ray.org = m_pos;
+        ray.dir = normalize(sscx * m_xAxis + sscy * m_yAxis + m_focus * m_dir);
+        // assign hit distance to infinity
+        ray.t = std::numeric_limits<float>::max();
+
 		return true;
 	}
 
@@ -40,6 +61,7 @@ private:
 	Vec3f m_pos;
 	Vec3f m_dir;
 	Vec3f m_up;
+	float m_angle;
 
 	// preprocessed values
 	float m_focus;
